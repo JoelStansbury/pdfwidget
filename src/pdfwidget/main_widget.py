26d55f94356bd2d15069f3564ad7e35c4f185712
JoelStansbury <48299585+JoelStansbury@users.noreply.github.com>
2020-03-21 18:44:36 -0400

    Add files via upload

--- src/pdfwidget/main_widget.py
+++ src/pdfwidget/main_widget.py
@@ -1,6 +1,6 @@
 import io
 
-from ipywidgets import Image
+from ipywidgets import Image, HTML
 from ipyevents import Event
 from pdf2image import (
     convert_from_path,
@@ -8,13 +8,13 @@ from pdf2image import (
 )
 
 
-def load_pdf(fname, dpi=None, **kwargs):
+def load_pdf(fname=None , img_list=None, **kwargs):
     """
     Given an path to a pdf or a bytes object
     return the pages as a list of png byte arrays
 
     :type fname: str, bytes
-    :param fname: Path to the pdf, or a Byte array of a pre-loaded pdf
+    :param fname:  Path to the pdf or  Byte array of a pre-loaded pdf
 
     :type dpi: int
     :param dpi: -> Image quality in DPI
@@ -41,46 +41,78 @@ def load_pdf(fname, dpi=None, **kwargs):
     grayscale -> Output grayscale image(s)
     """
     fmt = kwargs.get("fmt", "bmp")
-
     # convert pdf into a list of PIL images
-    if not dpi is None:
-        kwargs["dpi"] = dpi
     if isinstance(fname, str):
         pil_imgs = convert_from_path(fname, **kwargs) 
     elif isinstance(fname, bytes):
-        pil_imgs = convert_from_bytes(fname, **kwargs) 
+        pil_imgs = convert_from_bytes(fname, **kwargs)
+    else:
+        raise(ValueError(f"Unsuported type: {type(fname)}"))
 
     pages = []
     for i in pil_imgs:
         imgByteArr = io.BytesIO()
         i.save(imgByteArr, format=fmt)
         pages.append(imgByteArr.getvalue())
-    return pages
+    return pages, pil_imgs
 
 
 class PdfViewer(Image):
-    def __init__(self, fname, dpi, *args, **kwargs):
+    def __init__(self, fname=None, page_nav="click", *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.page_index = 0
-        self.pages = load_pdf(fname, dpi, *args, **kwargs)
-        self.value = self.pages[0]
-        
-        # Event Handlers
+        self.msg = HTML()
+
+        if fname:
+            self.load(fname, *args, **kwargs)
+    
+        paging_opts = {
+            "click": self.construct_click_handler,
+            "arrows": self.construct_arrow_handler
+        }
+
+        # Run the selected event constructor
+        paging_opts[page_nav]()
+
+    def construct_click_handler(self):
+        """ Should only be run once """
         disp_events = Event(
             source=self, 
-            watched_events=['click', 'keydown', 'mouseenter']
+            watched_events=['click']
         )
+        
+        def handle_disp(event):
+            if event["type"] == "click":
+                x = self.images[self.page_index]._size[0]
+                if event["relativeX"] > (x/2):
+                    self.next_page()
+                elif event["relativeX"] < (x/2):
+                    self.previous_page()
+        
+        disp_events.on_dom_event(handle_disp)
 
-        key_funcs = {
-            "ArrowLeft": self.previous_page, 
-            "ArrowRight": self.next_page,
-        }
+
+    def construct_arrow_handler(self):
+        """ Should only be run once """
+        disp_events = Event(
+            source=self, 
+            watched_events=['keydown']
+        )
         
         def handle_disp(event):
             if event["type"] == "keydown":
-                key_funcs[event["key"]]()
+                self.key_funcs[event["key"]]()
 
         disp_events.on_dom_event(handle_disp)
+
+
+    def load(self, fname, *args, **kwargs):
+        self.page_index = 0
+        self.pages, self.images = load_pdf(fname, *args, **kwargs)
+        self.value = self.pages[0]
+        self.key_funcs = {
+            "ArrowLeft": self.previous_page, 
+            "ArrowRight": self.next_page,
+        }
 
 
     def previous_page(self):
